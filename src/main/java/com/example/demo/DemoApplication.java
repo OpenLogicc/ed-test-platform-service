1e8f6c0e2748f6f61415c9c88536de80d689dc45
Md Athar <mdathar4403@example.com>
2026-08-23 13:30:38 +0530

    Implement authentication module

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,10 +13,5 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Testing Platform is up and running");
-
-
 	}
-
-
-
 }
